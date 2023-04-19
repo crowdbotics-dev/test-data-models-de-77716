1eec5c88f38b2b543ce3d27086a70b27cea6e6d6
Penny Wanjiru <penina@crowdbotics.com>
2023-04-19 17:43:48 +0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "test-data-models-de-77716",
-  url: "https://test-data-models-de-77716.botics.co",
-  api: "https://test-data-models-de-77716.botics.co/api/v1"
-}
+  "name": "test-data-models-de-77716",
+  "url": "https://test-data-models-de-77716.botics.co",
+  "api": "https://test-data-models-de-77716.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
--- store/testdatamodelsdeAPI/api.js
+++ store/testdatamodelsdeAPI/api.js
@@ -1,58 +1,72 @@
-import axios from "axios"
+import axios from "axios";
 const testdatamodelsdeAPI = axios.create({
   baseURL: "https://test-data-models-de-77716-prod.herokuapp.com",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+
 function api_v1_login_create(payload) {
-  return testdatamodelsdeAPI.post(`/api/v1/login/`, payload.data)
+  return testdatamodelsdeAPI.post(`/api/v1/login/`, payload.data);
 }
+
 function api_v1_signup_create(payload) {
-  return testdatamodelsdeAPI.post(`/api/v1/signup/`, payload.data)
+  return testdatamodelsdeAPI.post(`/api/v1/signup/`, payload.data);
 }
+
 function rest_auth_user_retrieve(payload) {
-  return testdatamodelsdeAPI.get(`/rest-auth/user/`)
+  return testdatamodelsdeAPI.get(`/rest-auth/user/`);
 }
+
 function rest_auth_user_update(payload) {
-  return testdatamodelsdeAPI.put(`/rest-auth/user/`, payload.data)
+  return testdatamodelsdeAPI.put(`/rest-auth/user/`, payload.data);
 }
+
 function rest_auth_user_partial_update(payload) {
-  return testdatamodelsdeAPI.patch(`/rest-auth/user/`, payload.data)
+  return testdatamodelsdeAPI.patch(`/rest-auth/user/`, payload.data);
 }
+
 function api_docs_schema_retrieve(payload) {
   return testdatamodelsdeAPI.get(`/api-docs/schema/`, {
-    params: { lang: payload.lang }
-  })
+    params: {
+      lang: payload.lang
+    }
+  });
 }
+
 function rest_auth_login_create(payload) {
-  return testdatamodelsdeAPI.post(`/rest-auth/login/`, payload.data)
+  return testdatamodelsdeAPI.post(`/rest-auth/login/`, payload.data);
 }
+
 function rest_auth_logout_retrieve(payload) {
-  return testdatamodelsdeAPI.get(`/rest-auth/logout/`)
+  return testdatamodelsdeAPI.get(`/rest-auth/logout/`);
 }
+
 function rest_auth_logout_create(payload) {
-  return testdatamodelsdeAPI.post(`/rest-auth/logout/`)
+  return testdatamodelsdeAPI.post(`/rest-auth/logout/`);
 }
+
 function rest_auth_registration_create(payload) {
-  return testdatamodelsdeAPI.post(`/rest-auth/registration/`, payload.data)
+  return testdatamodelsdeAPI.post(`/rest-auth/registration/`, payload.data);
 }
+
 function rest_auth_password_reset_create(payload) {
-  return testdatamodelsdeAPI.post(`/rest-auth/password/reset/`, payload.data)
+  return testdatamodelsdeAPI.post(`/rest-auth/password/reset/`, payload.data);
 }
+
 function rest_auth_password_change_create(payload) {
-  return testdatamodelsdeAPI.post(`/rest-auth/password/change/`, payload.data)
+  return testdatamodelsdeAPI.post(`/rest-auth/password/change/`, payload.data);
 }
+
 function rest_auth_password_reset_confirm_create(payload) {
-  return testdatamodelsdeAPI.post(
-    `/rest-auth/password/reset/confirm/`,
-    payload.data
-  )
+  return testdatamodelsdeAPI.post(`/rest-auth/password/reset/confirm/`, payload.data);
 }
+
 function rest_auth_registration_verify_email_create(payload) {
-  return testdatamodelsdeAPI.post(
-    `/rest-auth/registration/verify-email/`,
-    payload.data
-  )
+  return testdatamodelsdeAPI.post(`/rest-auth/registration/verify-email/`, payload.data);
 }
+
 export const apiService = {
   api_v1_login_create,
   api_v1_signup_create,
@@ -68,4 +82,4 @@ export const apiService = {
   rest_auth_password_change_create,
   rest_auth_password_reset_confirm_create,
   rest_auth_registration_verify_email_create
-}
+};
